705ee8277c8d9d304ef2634999e0d5344bc924e9
SarahEvelynn Johnson <sarah@5280startupsolutions.co>
2018-06-12 14:07:29 -0600

    fixed about me formatting

--- src/About/AboutMe.js
+++ src/About/AboutMe.js
@@ -4,43 +4,58 @@ import SarahEvelynnJohnsonResume from "./assets/SarahEvelynnJohnsonResume.pdf";
 import sej from "./assets/sej.jpg";
 import Header from "../Header/Header.js";
 
-
 export default class AboutMe extends React.Component {
   render() {
     return (
       <div>
-      <Header />
-      <div id="squareSection">
-        <div id="squareOne">
-          <h2 id="homeButton"> Welcome-Bienvenido! </h2>
-          <p>
-            Making a difference with integrity is what I build my life around. I am a native Coloradan with a deep love for the outdoors and the startup community.
-            <br/>
-            <br/>
-            When I am not at a startup event, coding or skiing, usually you can find me at a cafe or brewery with my dog, Benedict (named after Benedict Cumberbatch, of course).
-          </p>
+        <Header />
+        <div id="squareSection">
+          <div id="outterSquare">
+            <h2 id="homeButton"> Bienvenido! </h2>
+            <p>
+              Making a difference with integrity is what I build my life around.
+              I am a native Coloradan with a deep love for the outdoors and the
+              startup community.
+              <br />
+              <br />
+              When I am not at a startup event, coding or skiing, usually you
+              can find me at a cafe or brewery with my dog, Benedict (named
+              after Benedict Cumberbatch, of course).
+            </p>
             <div id="navagateButton">
-            <a href={"mailto:sarah@5280startupsolutions.co"} target="_blank">Contact Me</a>
-</div>
-        </div>
-        <div id="squareTwo">
-          <h2 id="homeButton"> What I Do </h2>
-          <p>
-            I specialize in creating the content that makes businesses run. This means helping businesses with their digital content creation (blogging, social media, deliverables) and market positioning (research, planning and implimentation).
-            <br />
-            <br />
-            Recently, I graduated from Galvanize, a six-month full-stack web immersive program. Now I am looking to dive into the world of development and for an opportunity to learn from an experienced developer.
-          </p>
-          <div id="resume">
-            <a id="navagateButton" href={SarahEvelynnJohnsonResume} download="SarahEvelynnJohnsonResume">
-              Download my Resume
-            </a>
+              <a href={"mailto:sarah@5280startupsolutions.co"} target="_blank">
+                Contact Me
+              </a>
+            </div>
+          </div>
+          <div id="innerSquare">
+            <h2 id="homeButton"> What I Do </h2>
+            <p>
+              I specialize in creating the content that makes businesses run.
+              This means helping businesses with their digital content creation
+              (blogging, social media, deliverables) and market positioning
+              (research, planning and implimentation).
+              <br />
+              <br />
+              Recently, I graduated from Galvanize, a six-month full-stack web
+              immersive program. Now I am looking to dive into the world of
+              development and for an opportunity to learn from an experienced
+              developer.
+            </p>
+            <div id="resume">
+              <a
+                id="navagateButton"
+                href={SarahEvelynnJohnsonResume}
+                download="SarahEvelynnJohnsonResume"
+              >
+                Download my Resume
+              </a>
+            </div>
+          </div>
+          <div id="outterSquare">
+            <img src={sej} id="sarahPhoto" alt="SarahEvelynn Johnson" />
           </div>
         </div>
-        <div id="squareThree">
-          <img src={sej} id="sarahPhoto" alt="SarahEvelynn Johnson" />
-        </div>
-      </div>
       </div>
     );
   }
--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import './App.css';
+import "./App.css";
 import Header from "./Header/Header.js";
 import Footer from "./Footer/Footer.js";
 import Section from "./frontPage/Index.js";
 import myWork from "./Work/Index.js";
-import AboutMe from "./About/AboutMe.js"
-
+import AboutMe from "./About/AboutMe.js";
 
 class App extends Component {
   render() {
@@ -15,14 +14,12 @@ class App extends Component {
         <Router>
           <div className="App-content">
             <Route exact path="/" component={Section} />
-<Route exact path="/myWork" component={myWork} />
-<Route exact path="/about" component={AboutMe} />
-
+            <Route exact path="/myWork" component={myWork} />
+            <Route exact path="/about" component={AboutMe} />
             <Footer />
           </div>
-      </Router>
+        </Router>
       </div>
-
     );
   }
 }

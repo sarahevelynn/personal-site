bb086ee927930ba0a74dfc05df86fb2a09ef399b
SarahEvelynn Johnson <sarah@5280startupsolutions.co>
2018-09-04 13:24:14 -0600

    new projects added

--- src/srcdesktop/Work/Projects.js
+++ src/srcdesktop/Work/Projects.js
@@ -2,9 +2,11 @@ import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import StartupWeekNav from "./Projects/startupWeekNav";
 import FiftyTwoEighty from "./Projects/fiftyTwoEighty";
+import SaveAway from "./Projects/saveAway";
 import SocialCloud from "./Projects/socialCloud";
 import Tracktivism from "./Projects/tracktivism";
 import InternConnect from "./Projects/internConnect";
+import PicShares from "./Projects/picShare";
 import BillTrack from "./Projects/billTrack";
 import DPT from "./Projects/DPT";
 import GoldCalendar from "./Projects/goldCalendar";
@@ -17,11 +19,13 @@ export class Projects extends React.Component {
         <div id="webProjects">
           <StartupWeekNav />
           <SocialCloud />
+          <SaveAway />
         </div>
 
         <div id="webProjects">
           <FiftyTwoEighty />
           <Tracktivism />
+          <PicShares />
         </div>
 
         <div id="webProjects">
